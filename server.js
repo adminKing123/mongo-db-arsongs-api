65de2317c94ec44108c86e3eb197e8c6b8d7326c
ils-harshn <127953102+ils-harshn@users.noreply.github.com>
2024-09-09 18:38:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://arythmlite.netlify.app"],
     optionsSuccessStatus: 200,
   })
 );
