2a7076c6b6a3cb5a26f39d92f300e74396a63181
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-05 14:48:00 +0530

    endpoint changes

--- arsongsapi/urls.py
+++ arsongsapi/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin panel
-    path('api/', include('songs.urls')),  # Include the URLs from the songs app
+    path('content/', include('songs.urls')),  # Include the URLs from the songs app
 ]
