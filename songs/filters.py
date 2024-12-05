eb5af364ef408895409cb42af9e22e146777c92d
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-05 15:05:26 +0530

    more filters

--- songs/filters.py
+++ songs/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Song
+from .models import Song, Artist, Album, Tag
 
 class SongFilter(django_filters.FilterSet):
     original_name = django_filters.CharFilter(field_name='original_name', lookup_expr='icontains')
@@ -10,3 +10,26 @@ class SongFilter(django_filters.FilterSet):
     class Meta:
         model = Song
         fields = ['original_name', 'album_id', 'artist_ids', 'tag_ids']
+
+class ArtistFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = Artist
+        fields = ['name']
+
+class TagFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+class AlbumFilter(django_filters.FilterSet):
+    code = django_filters.CharFilter(field_name='code', lookup_expr='icontains')
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    year = django_filters.NumberFilter(field_name='year')
+
+    class Meta:
+        model = Album  # Correct the model to 'Album'
+        fields = ['code', 'title', 'year']
--- songs/views.py
+++ songs/views.py
@@ -1,22 +1,27 @@
 from rest_framework import viewsets
 from .models import Album, Artist, Tag, Song
 from .serializers import AlbumSerializer, ArtistSerializer, TagSerializer, SongSerializer
-from .filters import SongFilter
-
 from django_filters.rest_framework import DjangoFilterBackend
+from .filters import SongFilter, ArtistFilter, AlbumFilter, TagFilter
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AlbumFilter
 
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ArtistFilter
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TagFilter
 
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
