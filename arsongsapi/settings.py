776643bad6ca8581d970f85f6df69ed7f15cc875
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-12-09 17:46:26 +0530

    possible

--- arsongsapi/settings.py
+++ arsongsapi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
@@ -46,6 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = CONFIG["REST_FRAMEWORK"]
 
 STATIC_ROOT = CONFIG['COLLECT_STATIC_PATH']
+CORS_ALLOWED_ORIGINS = CONFIG['CORS_ALLOWED_ORIGINS']
